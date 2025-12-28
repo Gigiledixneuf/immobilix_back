9c052c64a122e5b6eb1058f36346e6317ce80cbe
AlbusDigi <albus.digit@gmail.com>
2025-12-28 03:00:20 +0100

    feat: ajout des modeles PropertyView et Favorite
    - Modele PropertyView pour tracker les consultations de proprietes
    - Modele Favorite pour gerer les proprietes favorites
    - Relations avec User et Property
    - Support des timestamps automatiques

---
+++ app/models/favorite.ts
@@ -0,0 +1,28 @@
+import { DateTime } from 'luxon'
+import { BaseModel, belongsTo, column } from '@adonisjs/lucid/orm'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+import Property from '#models/property'
+import User from '#models/user'
+
+export default class Favorite extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare propertyId: number
+
+  @column()
+  declare userId: number
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+
+  @belongsTo(() => Property)
+  declare property: BelongsTo<typeof Property>
+
+  @belongsTo(() => User)
+  declare user: BelongsTo<typeof User>
+}
